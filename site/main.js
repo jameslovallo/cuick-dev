262522cc75c751faa70e0fa231c58fc2d93ba20e
James Lovallo <james.lovallo@thrivent.com>
2024-01-12 22:04:17 -0500

    remove docs content from site

--- site/main.js
+++
@@ -1,9 +0,0 @@
-// core components
-import '/components/code.js'
-import '/components/spa.js'
-import '/components/story.js'
-// site-specific components
-import '/site/components/counter.js'
-import '/site/components/currency.js'
-import '/site/components/i18n.js'
-import '/site/components/nav.js'
